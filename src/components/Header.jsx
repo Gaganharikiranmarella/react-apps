7202940fe001c5d0378346eb066b7359d8b0f325
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-23 20:06:32 +0530

    modified Header files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
+
 export default function Header() {
   return (
     <div className="App-Header-Row">
-      <h1 style={{backgroundColor:'yellow',color:'blue'}}>React Lessons</h1>
-      <Link to="/">App1</Link>-<Link to="/app2">App2</Link>
+      <h1>React Lessons</h1>
+      <div className="nav-links">
+        <Link to="/">App1</Link>
+        <Link to="/app2">App2</Link>
+      </div>
     </div>
   );
 }
