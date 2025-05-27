ac9f3cd001c204655001cd8be48b082f91214194
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-27 20:43:22 +0530

    added App21

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import App16 from "./components/App16";
 import App17 from "./components/App17";
 import App19 from "./components/App19";
 import App20 from "./components/App20";
+import App21 from "./components/App21";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -38,18 +39,19 @@ function App() {
         <Route path="/app5" element={<App5 />} />
         <Route path="/app6" element={<App6 />} />
         <Route path="/app7" element={<App7 />} />
-        <Route path="/app8" element={<App8 />} /> 
-        <Route path="/app9" element={<App9 />} /> 
-        <Route path="/app10" element={<App10 />} /> 
-        <Route path="/app11" element={<App11 />} /> 
-         <Route path="/app12" element={<App12 />} /> 
-        <Route path="/app13" element={<App13 />} /> 
-        <Route path="/app14" element={<App14 />} /> 
-        <Route path="/app15" element={<App15 />} /> 
-        <Route path="/app16" element={<App16 />} /> 
-         <Route path="/app17" element={<App17 />} /> 
-         <Route path="/app19" element={<App19 />} />
-         <Route path="/app20" element={<App20 />} /> 
+        <Route path="/app8" element={<App8 />} />
+        <Route path="/app9" element={<App9 />} />
+        <Route path="/app10" element={<App10 />} />
+        <Route path="/app11" element={<App11 />} />
+        <Route path="/app12" element={<App12 />} />
+        <Route path="/app13" element={<App13 />} />
+        <Route path="/app14" element={<App14 />} />
+        <Route path="/app15" element={<App15 />} />
+        <Route path="/app16" element={<App16 />} />
+        <Route path="/app17" element={<App17 />} />
+        <Route path="/app19" element={<App19 />} />
+        <Route path="/app20" element={<App20 />} />
+        <Route path="/app21" element={<App21 />} />
       </Routes>
       <Footer />
     </BrowserRouter>
